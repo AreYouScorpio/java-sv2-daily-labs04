37f0eb1cf30dead0409a50e0728b67e9ba8d07bd
AreYouScorpio <akos.schweger@gmail.com>
2021-11-04 17:56:05 +0100

    day03 feladat .. working

--- src/main/java/day03/main.java
+++ src/main/java/day03/main.java
@@ -2,13 +2,13 @@ package day03;
 
 public class main {
 
-    /* public static void main(String[] args) {
+    /*public static void main(String[] args) {
         Stock stock = new Stock (Arrays.asList(1.23,4.35,0.23,4.0,8.4));
         System.out.println(stock.maxProfit()); //8.17
 
         Stock stock2 = new Stock(Arrays.aslist(12.0,2.0,8.0, 4.0));
         System.out.println(stock2.maxProfit()); //6.0
     }
+*/
 
-     */
 }
